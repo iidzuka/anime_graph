bbd1ffc9f26d2b35a310fd7b03319902e20d7dd2
iidzuka <taki.hiyoshi@gmail.com>
2016-11-22 02:47:32 +0900

    add eslint and eslint refactoring #1

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+import eslint from 'gulp-eslint';
+
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const del = require('del');
 const exec = require('child_process').exec;
 
+
 const paths = {
   allSrcJs: 'src/**/*.js',
+  gulpFile: 'gulpfile.babel.js',
   libDir: 'lib',
 };
 
-gulp.task('clean', () => {
-  return del(paths.libDir);
-});
+gulp.task('clean', () => del(paths.libDir));
 
-gulp.task('build', ['clean'], () => {
-  return gulp.src(paths.allSrcJs)
+gulp.task('build', ['lint', 'clean'], () =>
+  gulp.src(paths.allSrcJs)
     .pipe(babel())
-    .pipe(gulp.dest(paths.libDir));
-});
+    .pipe(gulp.dest(paths.libDir)),
+);
 
 gulp.task('main', ['build'], (callback) => {
   exec(`node ${paths.libDir}`, (error, stdout) => {
@@ -30,3 +34,13 @@ gulp.task('watch', () => {
 });
 
 gulp.task('default', ['watch', 'main']);
+
+gulp.task('lint', () =>
+  gulp.src([
+    paths.allSrcJs,
+    paths.gulpFile,
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError()),
+);
